d96ef1cd727cf82a7c5cb14fb79f2f647e5fa4fe
Amanda Stensland <amanda.stensland@bonniernews.se>
2021-06-02 19:18:25 +0200

    Update document title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>Sunstudies</title>
         <link href="{{asset('css/app.css')}}" rel="stylesheet">
         <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
